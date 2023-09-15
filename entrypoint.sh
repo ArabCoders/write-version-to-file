07e936dbe22810523e89d20c927b531c64955b57
Abdulmhsen B. A. A <admin@arabcoders.org>
2023-09-15 16:32:29 +0300

    Added date to automatic version.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ log() {
 
 filename="/github/workspace/$1"
 
-log "File Name:" "$filename"
+log "File Name:" "${filename}"
 
 if [ -z "$2" ]; then
     placeholder="\${VERSION}"
@@ -19,19 +19,20 @@ else
     placeholder=$2
 fi
 
-log "Placeholder:" "$placeholder"
+log "Placeholder:" "${placeholder}"
 
-if test -f "$filename"; then
-    content=$(cat "$filename")
+if test -f "${filename}"; then
+    content=$(cat "${filename}")
 else
-    error "Version file not found! Looked for:" "$filename"
+    error "Version file not found! Looked for:" "${filename}"
     exit 1;
 fi
 
 git fetch --tags --force
 latestVersionTag=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
+userTag="$(date -u +'%Y%m%d')-${latestVersionTag}"
 
-log "Replacing placeholder with:" "$latestVersionTag"
+log "Replacing placeholder with: $(userTag)"
 
-updatedContent=$(< "$filename" sed "s/$placeholder/$latestVersionTag/g")
-echo "$updatedContent" > "$filename"
+updatedContent=$(< "$filename" sed "s/${placeholder}/${userTag}/g")
+echo "${updatedContent}" > "${filename}"
