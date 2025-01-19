ab8e006e1b94bc3a688f766b7bf4e5b148922c8a
ArabCoders <admin@arabcoders.org>
2025-01-19 20:14:24 +0300

    added with_branch option

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,26 +25,40 @@ else
     NO_DATE=${3}
 fi
 
+if [ -z "${4}" ]; then
+    WITH_BRANCH='false'
+else
+    WITH_BRANCH=${4}
+fi
+
 log "Placeholder:" "${placeholder}"
 log "NO Date:" "${NO_DATE}"
+log "Branch Prefix:" "${WITH_BRANCH}"
 
 if test -f "${filename}"; then
     content=$(cat "${filename}")
 else
     error "Version file not found! Looked for:" "${filename}"
-    exit 1;
+    exit 1
 fi
 
 git fetch --tags --force
-latestVersionTag=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
+latestVersionTag=$(git describe --exact-match --tags 2>/dev/null || git rev-parse --short HEAD)
 
-if [ "${NO_DATE}" == 'true' ]; then
+if [ "${NO_DATE}" = 'true' ]; then
     userTag="${latestVersionTag}"
 else
     userTag="$(date -u +'%Y%m%d')-${latestVersionTag}"
 fi
 
+# If branch prefixing is enabled, retrieve the branch name and prefix the userTag.
+if [ "${WITH_BRANCH}" = 'true' ]; then
+    branchName=$(echo "$GITHUB_REF" | sed 's#refs/heads/##')
+    userTag="${branchName}-${userTag}"
+fi
+
 log "Replacing placeholder with: ${userTag}"
 
-updatedContent=$(< "$filename" sed "s/${placeholder}/${userTag}/g")
-echo "${updatedContent}" > "${filename}"
+# Use sed to replace the placeholder with the new tag.
+updatedContent=$(sed "s/${placeholder}/${userTag}/g" "$filename")
+echo "${updatedContent}" >"${filename}"
